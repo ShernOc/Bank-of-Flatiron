3e827c286a08c40f743816b647aed480462a960c
Sherlyne <sherlynea1994@hotmail.com>
2024-08-12 13:17:58 +0300

    code challenge

---
+++ src/components/Transaction.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Transactions from "./db.json";
+
+function Transaction() {
+
+function GetTransaction(){
+  
+}
+ 
+ 
+ 
+ 
+ 
+  return (
+
+
+    // Have a table 
+    <tr>
+      {/* Headings  */}
+      <td>
+       tr
+      </td>
+      {/*  */}
+      <td>{"your code here..."}</td>
+      <td>{"your code here..."}</td>
+      <td>{"your code here..."}</td>
+    </tr>
+  );
+}
+
+export default Transaction;
